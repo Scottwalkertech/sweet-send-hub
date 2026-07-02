3e1c1e31c8330a8e6366d071d1274402670ede3a
Scottwalkertech <scottwalkertech@gmail.com>
2026-07-02 05:16:19 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackBuildConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  build: {
-    outDir: ".output/public",
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       "@": "/src",
     },
   },
+  nitro: {
+    preset: "static"
+  }
 });
